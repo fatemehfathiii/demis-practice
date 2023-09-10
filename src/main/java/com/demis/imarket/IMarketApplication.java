3401a99f53a091b5f3697952a5fcce9030cb4ece
fatemeh.fathi <fatemehfathi409@gmail.com>
2023-09-10 12:12:46 +0330

    add pom file

---
+++ src/main/java/com/demis/imarket/IMarketApplication.java
@@ -0,0 +1,13 @@
+package com.demis.imarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demis/imarket/IMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demis.imarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
